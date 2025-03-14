d6bde98b6991bc5ae05587c2834f0ef3596e2491
Veronica Shyian <veronica.shyian@gmail.com>
2025-03-14 20:40:06 +0200

    BugFix

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,6 @@ export const App = () => {
           {goods.map(good => (
             <tr
               data-cy="Good"
-              key={good}
               className={
                 selectedGood === good ? 'has-background-success-light' : ''
               }
